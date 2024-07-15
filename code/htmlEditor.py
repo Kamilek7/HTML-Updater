d8c85be97ed1734478845cde85646d106323e777
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2024-07-15 23:06:12 +0200

    jakis wstep do obrobki html

--- code/htmlEditor.py
+++ code/htmlEditor.py
@@ -1,13 +1,41 @@
+from os.path import *
 class htmlEdit:
-    def __init__(self):
+    mainCode = ""
+    mainDir = ""
+    fillingCode = ""
+    fillingDir = ""
+    dataDir = ""
+    data = {}
+
+    @staticmethod
+    def loadPaths():
+        thisDir = abspath(__file__)
+        template = join(dirname(dirname(thisDir)), "template")
+        htmlEdit.mainDir = join(template, "main", "index.html")
+        htmlEdit.fillingDir = join(template, "gallery", "window.html")
+        resources = join(dirname(dirname(thisDir)), "resources")
+
+    @staticmethod
+    def loadHTML():
+        file = open(htmlEdit.mainDir, "r")
+        htmlEdit.mainCode = file.read()
+        file.close()
+        file = open(htmlEdit.fillingDir, "r")
+        htmlEdit.fillingCode = file.read()
+        file.close()
+
+    @staticmethod
+    def loadImages():
         pass
 
-
-
-    def loadHTML(self):
-        file = open(self.mainDir, "r")
-        self.main = file.read()
-        self.template = open(self.templateDir, "r")
-
-    def fillTemplate(self):
+    @staticmethod
+    def loadData():
         pass
+
+    @staticmethod
+    def fillTemplate():
+        pass
+
+htmlEdit.loadPaths()
+htmlEdit.loadHTML()
+print(htmlEdit.mainCode)
