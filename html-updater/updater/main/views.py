f3f8c00c7a9c92db16ea68f6f366fcc58eef9c27
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2024-07-15 19:22:57 +0200

    nie bedzie jednak django bo nie bedzie do niczego potrzebne

--- html-updater/updater/main/views.py
+++
@@ -1,6 +0,0 @@
-from django.template import loader
-from django.http import HttpResponse
-
-def main(request):
-    template = loader.get_template('test.html')
-    return HttpResponse(template.render())
