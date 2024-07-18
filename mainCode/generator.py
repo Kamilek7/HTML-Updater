f77cca0dc979de675ca4bad8f828922355f1bb90
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2024-07-18 23:25:40 +0200

    cos sobie robie z generator.py

--- mainCode/generator.py
+++ mainCode/generator.py
@@ -1,3 +1,8 @@
 from htmlEditor import *
-print("Tutaj jakies info aaaaa")
+szerokosc = 10
+wiadomosc = "Witaj w progamie sluzacym do generowania gotowych projektow HTML na podstawie odpowiednich szablonow i ustawień wypisanych w załączonym pliku .JSON. Program jak na razie kompletnie raczkuje i został stworzony z zamysłem obsługi go przez odpowiedni serwer (np web server Django ze swoim 'gui')."
+podkreslenia = "="* szerokosc
+wiadomosc = podkreslenia+ "\n" + wiadomosc + "\n" + podkreslenia
+wiadomosc.rjust(szerokosc," ")
+print(wiadomosc)
 htmlEdit.createOutputFiles()
