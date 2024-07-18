364053360733bb1887fbd6ddf09a0b5a3820d53e
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2024-07-18 23:45:37 +0200

    wyjustowanie tekstu

--- mainCode/generator.py
+++ mainCode/generator.py
@@ -2,7 +2,23 @@ from htmlEditor import *
 szerokosc = 40
 wiadomosc = "Witaj w progamie sluzacym do generowania gotowych projektow HTML na podstawie odpowiednich szablonow i ustawień wypisanych w załączonym pliku .JSON. Program jak na razie kompletnie raczkuje i został stworzony z zamysłem obsługi go przez odpowiedni serwer (np web server Django ze swoim 'gui')."
 podkreslenia = "="* szerokosc
+slowa = wiadomosc.split()
+wiadomosc = ""
+i = 0
+linijka = ""
+while i < len(slowa):
+    slowo = slowa[i] + " "
+    if len(slowo) + len(linijka) <= szerokosc:
+        linijka+=slowo
+        i+=1
+    else:
+        linijka = linijka.ljust(szerokosc, " ")
+        wiadomosc += linijka + "\n"
+        linijka = ""
+wiadomosc += linijka
+
+
 wiadomosc = podkreslenia+ "\n" + wiadomosc + "\n" + podkreslenia
-wiadomosc.rjust(szerokosc," ")
+
 print(wiadomosc)
 htmlEdit.createOutputFiles()
