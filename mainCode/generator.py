259f5b5f734bdc07a7320ba1631c48e1c72b1616
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2024-07-18 23:26:37 +0200

    proba wyjustowania tekstu powitalnego

--- mainCode/generator.py
+++ mainCode/generator.py
@@ -1,5 +1,5 @@
 from htmlEditor import *
-szerokosc = 10
+szerokosc = 40
 wiadomosc = "Witaj w progamie sluzacym do generowania gotowych projektow HTML na podstawie odpowiednich szablonow i ustawień wypisanych w załączonym pliku .JSON. Program jak na razie kompletnie raczkuje i został stworzony z zamysłem obsługi go przez odpowiedni serwer (np web server Django ze swoim 'gui')."
 podkreslenia = "="* szerokosc
 wiadomosc = podkreslenia+ "\n" + wiadomosc + "\n" + podkreslenia
