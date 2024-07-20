f67d8600b602c08d6adfd7d3db2ccdc0d0f242c3
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2024-07-20 17:54:21 +0200

    test analizy dziala

--- mainCode/htmlAnalyser.py
+++ mainCode/htmlAnalyser.py
@@ -1,20 +1,7 @@
 from htmlLoader import *
 class HtmlAnalyser:
+
     @staticmethod
-    def extractCoding(code):
-        codeCpy = code
-        if code.count("*#") == codeCpy.count("#*") and codeCpy.count("*#")>0:
-            numberOfCodings = codeCpy.count("*#")
-            codes = []
-            for i in range (numberOfCodings):
-                startIdx = codeCpy.find("*#")
-                endIdx = codeCpy.find("#*")
-                tempCode = codeCpy[startIdx+2:endIdx-2]
-                codes.append(tempCode)
-            return codes
-        else:
-            raise OSError("Plik ze ścieżki " + dir + " jest źle okodowany lub w części uzytkowej wystąpily znaki kodujące (*# i #*).")
-        
     def analyzeCoding(codes):
         if not "*#" in codes and not "#*" in codes:
             codesD = []
@@ -23,7 +10,30 @@ class HtmlAnalyser:
             return codesD
         else:
             raise OSError("W przetworzonym przez extractCoding kodzie znaleziono znaki kodujace (*# i #*).")
+        
+    @staticmethod
+    def extractCoding(code):
+        codeCpy = code
+        if codeCpy.count("*#") == codeCpy.count("#*") and codeCpy.count("*#")>0:
+            numberOfCodings = codeCpy.count("*#")
+            codes = []
+            for i in range (numberOfCodings):
+                startIdx = codeCpy.find("*#")
+                endIdx = codeCpy.find("#*")
+                tempCode = codeCpy[startIdx+2:endIdx]
+                codes.append(tempCode)
+                codeCpy = codeCpy[endIdx+2:-1]
+            codes = HtmlAnalyser.analyzeCoding(codes)
+            return codes
+        else:
+            raise OSError("Plik ze ścieżki " + dir + " jest źle okodowany lub w części uzytkowej wystąpily znaki kodujące (*# i #*).")
+        
+
     
     @staticmethod
     def getHTMLInfo(dir):
         code = FileManager.loadHTML(dir)
+        codes = HtmlAnalyser.extractCoding(code)
+        print(codes)
+
+HtmlAnalyser.getHTMLInfo("C:\\Users\\kamil\\Documents\\projekty\\nieskonczone\\Python\\HTML-Updater\\template\\main\\index.html")
