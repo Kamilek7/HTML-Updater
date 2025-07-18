eede807fcbfce28b30a27d66d5841fadb35693af
kamilek7 <kamijar20031@gmail.com>
2025-07-18 21:13:55 +0200

    python fix

--- mainCode/fromOutputToMainSite.py
+++ mainCode/fromOutputToMainSite.py
@@ -3,13 +3,13 @@ from constants import *
 with open(os.path.join(MAIN_DIR, "config.json"), 'r') as jsoner:
     data = json.load(jsoner)
     location = data["pagePath"]
-    if os.exists(os.path.join(location, "imgs")):
+    if os.path.exists(os.path.join(location, "imgs")):
         shutil.rmtree(os.path.join(location, "imgs"))
     shutil.copytree(os.path.join(OUTPUT_DIR, "imgs"),os.path.join(location, "imgs"))
     files = ["index.html", "main.js", "styles.css"]
     for file in files:
-        if os.exists(os.path.join(location, file)):
+        if os.path.exists(os.path.join(location, file)):
             os.remove(os.path.join(location, file))
-        if os.exists(os.path.join(OUTPUT_DIR, file)):
+        if os.path.exists(os.path.join(OUTPUT_DIR, file)):
             shutil.copyfile(os.path.join(OUTPUT_DIR, file), os.path.join(location, file))
             os.remove(os.path.join(OUTPUT_DIR, file))
